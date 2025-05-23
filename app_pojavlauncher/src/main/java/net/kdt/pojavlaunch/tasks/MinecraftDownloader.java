bbd2f967fefaa378359c1aacca0dd0231b52de45
Hitomi <hitomilovely8@gmail.com>
2025-05-23 14:16:27 +0800

    Update MinecraftDownloader.java

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/tasks/MinecraftDownloader.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/tasks/MinecraftDownloader.java
@@ -55,9 +55,9 @@ public class MinecraftDownloader {
 
     private static final ThreadLocal<byte[]> sThreadLocalDownloadBuffer = new ThreadLocal<>();
 
-    private boolean isLocalProfile = false;
+    /* private boolean isLocalProfile = false;
 
-    /**
+
      * Start the game version download process on the global executor service.
      * @param activity Activity, used for automatic installation of JRE 17 if needed
      * @param version The JMinecraftVersionList.Version from the version list, if available
@@ -67,13 +67,13 @@ public class MinecraftDownloader {
     public void start(@Nullable Activity activity, @Nullable JMinecraftVersionList.Version version,
                       @NonNull String realVersion, // this was there for a reason
                       @NonNull AsyncMinecraftDownloader.DoneListener listener) {
-        if(activity != null){
+        /* if(activity != null){
             isLocalProfile = Tools.isLocalProfile(activity);
             Tools.switchDemo(Tools.isDemoProfile(activity));
         } else {
             isLocalProfile = true;
             Tools.switchDemo(true);
-        }
+        } */
 
         sExecutorService.execute(() -> {
             try {
